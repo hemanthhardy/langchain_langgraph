43239ad3f6f678fa52da2ad9771a9e6df2d22c24
Hemanth Kumar <hemanthkumar353535@gmail.com>
2025-11-25 19:07:39 +0530

    Add files via upload

---
+++ Embeddings & RAG/1_hf_embeddings.py
@@ -0,0 +1,22 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+documents = [
+    "The Eiffel Tower is located in Paris.",
+    "The capital of Germany is Berlin.",
+    "The Great Wall of China is a historic landmark.",
+    "The capital of France is Paris.",
+    "Mount Everest is the highest mountain in the world.",
+    "The capital of Italy is Rome." 
+]
+query = "What is the capital of France?"
+query_embedding = embedding_model.embed_query(query)
+doc_embeddings = embedding_model.embed_documents(documents)
+# print(doc_embeddings)
+similarities = cosine_similarity([query_embedding], doc_embeddings)
+print(similarities)
+most_similar_doc_index = similarities.argmax()
+most_similar_document = documents[most_similar_doc_index]
+print("Most similar document:", most_similar_document)
