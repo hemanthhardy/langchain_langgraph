facaebbd18c82ceec7eb6efbe8342b6c6e2f61e6
Hemanth Kumar <hemanthkumar353535@gmail.com>
2025-11-26 11:30:59 +0530

    Integrate ChatGoogleGenerativeAI and update documents
    Added Google Generative AI model integration and updated the capital of France in the documents list.

--- Embeddings & RAG/1_hf_embeddings.py
+++ Embeddings & RAG/1_hf_embeddings.py
@@ -1,13 +1,18 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 documents = [
     "The Eiffel Tower is located in Paris.",
     "The capital of Germany is Berlin.",
     "The Great Wall of China is a historic landmark.",
-    "The capital of France is Paris.",
+    "The capital of France is Delhi.",
     "Mount Everest is the highest mountain in the world.",
     "The capital of Italy is Rome." 
 ]
@@ -20,3 +25,9 @@ print(similarities)
 most_similar_doc_index = similarities.argmax()
 most_similar_document = documents[most_similar_doc_index]
 print("Most similar document:", most_similar_document)
+
+query = query +'\nrefer the below data to answer:\n'+  most_similar_document
+print('\n\nModel Input: \n', query,'\n')
+result =  model.invoke(query)
+# print(result.content)
+print(result)
